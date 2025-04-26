1f50ae31b64382ece1df1f390cfdcc14b2aaf7b8
Shahriyor Adhamov <shahriyoradhamov000@gmail.com>
2025-04-26 14:40:07 +0500

    feat: add copy  functionality from fs

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,25 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
 const copy = async () => {
-    // Write your code here 
+  const filePath = join('files');
+  const copyFilePath = join('files_copy');
+
+  try {
+    await fs.access(filePath);
+    try {
+      await fs.access(copyFilePath);
+      throw new Error('FS operation failed');
+    } catch (error) {
+      if (error.code !== 'ENOENT') {
+        throw new Error('FS operation failed');
+      }
+    }
+    await fs.cp(filePath, copyFilePath, { recursive: true });
+    console.log('Folder copied successfully');
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
