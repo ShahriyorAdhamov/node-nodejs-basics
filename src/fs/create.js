88d9b148a60570d4b4eade58a46504daa03655dd
Shahriyor Adhamov <shahriyoradhamov000@gmail.com>
2025-04-26 14:02:38 +0500

    feat: add create functionality

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const filePath = join('files', 'fresh.txt');
+
+  try {
+    await fs.access(filePath);
+    throw new Error('FS operation failed');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await fs.writeFile(filePath, 'I am fresh and young', { flag: 'wx' });
+      console.log('File created successfully.');
+    } else {
+      throw new Error('FS operation failed');
+    }
+  }
 };
 
 await create();
