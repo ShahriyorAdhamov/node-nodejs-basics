baecb6c0f419d71396f376cd83e1b22982af68dc
Shahriyor Adhamov <shahriyoradhamov000@gmail.com>
2025-04-26 21:56:24 +0500

    feat: add rename functionality from fs

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
 const rename = async () => {
-    // Write your code here 
+  const oldFilePath = join('files', 'wrongFilename.txt');
+  const newFilePath = join('files', 'properFilename.md');
+
+  try {
+    await fs.access(oldFilePath);
+    try {
+      await fs.access(newFilePath);
+      throw new Error('FS operation failed');
+    } catch (error) {
+      if (error.code !== 'ENOENT') {
+        throw new Error('FS operation failed');
+      }
+    }
+    
+    await fs.rename(oldFilePath, newFilePath);
+    console.log('File renamed successfully');
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
