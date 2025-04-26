90c828f297b164a2c9c13f89e9a7b216f4e57ab4
Shahriyor Adhamov <shahriyoradhamov000@gmail.com>
2025-04-26 21:41:47 +0500

    feat: add read functionality from fs

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { promises as fs } from 'fs';
+import { join } from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join('files', 'fileToRead.txt');
+  try {
+    await fs.access(filePath);
+    const res = await fs.readFile(filePath, 'utf-8');
+
+    console.log(res);
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
