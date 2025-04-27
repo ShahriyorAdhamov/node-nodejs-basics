17a3dc0faa3cee5746da839f3dccb7d8c53b02db
Shahriyor Adhamov <shahriyoradhamov000@gmail.com>
2025-04-27 23:12:27 +0500

    feat: add read functionality from streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,23 @@
+import { createReadStream } from 'fs';
+import { resolve } from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = resolve('files/fileToRead.txt');
+    const readStream = createReadStream(filePath, 'utf-8');
+
+    readStream.on('data', (chunk) => {
+        process.stdout.write(chunk);
+    });
+
+    readStream.on('error', (error) => {
+        console.error('Error reading file:', error);
+        throw error;
+    });
+
+    return new Promise((resolve, reject) => {
+        readStream.on('end', resolve);
+        readStream.on('error', reject);
+    });
 };
 
 await read();
