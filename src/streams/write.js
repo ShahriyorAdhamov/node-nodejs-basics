394f4995ac1e94e8d0b29f1eba686f2e201ed16e
Shahriyor Adhamov <shahriyoradhamov000@gmail.com>
2025-04-27 23:28:42 +0500

    feat: add write functionality from streams

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import { createWriteStream } from 'fs';
+import { resolve } from 'path';
+
 const write = async () => {
-    // Write your code here 
+    const filePath = resolve('files/fileToWrite.txt');
+    const writeStream = createWriteStream(filePath);
+
+    process.stdin.pipe(writeStream);
+
+    return new Promise((resolve, reject) => {
+        writeStream.on('finish', resolve);
+        writeStream.on('error', reject);
+    });
 };
 
 await write();
