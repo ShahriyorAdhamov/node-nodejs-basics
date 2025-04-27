1db33cf021a8b55e6cfe832ac609d4db2b632de2
Shahriyor Adhamov <shahriyoradhamov000@gmail.com>
2025-04-27 22:21:31 +0500

    feat: add env functionality from cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const rssVars = Object.entries(process.env)
+    .filter(([key]) => key.startsWith('RSS_'))
+    .map(([key, value]) => `${key}=${value}`)
+    .join('; ');
+
+  console.log(rssVars || 'No RSS_ variables found');
 };
 
 parseEnv();
