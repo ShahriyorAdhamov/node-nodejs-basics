41bfadb5b79b37c6bf79d0c64d300ac58bd063e4
Shahriyor Adhamov <shahriyoradhamov000@gmail.com>
2025-04-28 18:20:50 +0500

    feat: add calc hash functionality from hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+import { resolve } from 'path';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = resolve('files/fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+  const stream = createReadStream(filePath);
+
+  stream.on('error', (err) => {
+    console.error('Error reading file:', err.message);
+  });
+
+  stream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  stream.on('end', () => {
+    const finalHash = hash.digest('hex');
+    console.log(finalHash);
+  });
 };
 
 await calculateHash();
