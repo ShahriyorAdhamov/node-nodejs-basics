ac2f827c793ab0e74dd778d969b1b15710692d1e
Shahriyor Adhamov <shahriyoradhamov000@gmail.com>
2025-04-28 20:12:13 +0500

    feat: add cp functionality from cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
+import { spawn } from 'child_process';
+import { resolve } from 'path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = resolve('src/cp/files/script.js');
+
+  const child = spawn('node', [scriptPath, ...args], {
+    stdio: ['pipe', 'pipe', 'inherit']
+  });
+
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.pipe(process.stdout);
+
+  child.on('error', (err) => {
+    console.error('Failed to start child process:', err.message);
+  });
+
+  child.on('exit', (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2', 'arg3']);
